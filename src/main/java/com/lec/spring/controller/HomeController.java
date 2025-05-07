6fb5cb8460563e8e230157d85567699e106ed9d9
kkk523 <kim.hyungoo.i@gmail.com>
2025-05-07 14:37:52 +0900

    v1.1

--- src/main/java/com/lec/spring/controller/HomeController.java
+++ src/main/java/com/lec/spring/controller/HomeController.java
@@ -19,7 +19,7 @@ public class HomeController {
             log.warn("/aws/v1 호출. warning 로그");
         }
 
-        return "<h1>AWS 배포 v1</h1>";
+        return "<h1>AWS 배포 v1.1</h1>";
     }
 
 }
