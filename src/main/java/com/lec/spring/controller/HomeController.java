119f9f65c7d2ddba2bba8d3194a300c893542934
kkk523 <kim.hyungoo.i@gmail.com>
2025-05-07 10:04:22 +0900

    init

---
+++ src/main/java/com/lec/spring/controller/HomeController.java
@@ -0,0 +1,39 @@
+package com.lec.spring.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController  // body 을 리턴
+public class HomeController {
+
+    @GetMapping("/aws/v1")
+    public String hello (@RequestParam(defaultValue = "1") Integer num) {
+        if(num == 1) {
+            log.info("/aws/v1 호출. info 로그");
+        } else if (num == -1) {
+            log.error("/aws/v1 호출. error 로그");
+        } else if (num == 0){
+            log.warn("/aws/v1 호출. warning 로그");
+        }
+
+        return "<h1>AWS 배포 v1</h1>";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
